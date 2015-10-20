95ec068deaaab15346fc05ef5af608cebf47bf9d
root <root@template>
2015-10-20 11:29:06 -0200

    acertei os comandos do capybara para limitar o escopo dos método, evitando redundancias

--- app/models/linux_printer.rb
+++ app/models/linux_printer.rb
@@ -6,24 +6,24 @@ Capybara.default_driver = :poltergeist
 class LinuxPrinter < ActiveRecord::Base
 
   def self.get_status
-    visit "http://euclides/"
+    page.visit "http://euclides/"
     euclides = page.first("span[class='StatusMessage']").text
 
     if euclides =~ (/error/i)
-      visit "http://euclides/dstatus.cgi"
+      page.visit "http://euclides/dstatus.cgi"
       page.click_button("Error Information")
       euclides = page.all("tbody")[2].text
 
     end
 
-    visit "http://galois/"
+    page.visit "http://galois/"
     galois = page.all("td[height='26']")[2].text
 
     if galois =~ (/error/i)
-      find("input[type$='text']").set '2'
-      find("a[onclick*='login()']").click
-      visit "http://galois/dev_error.html"
-      galois = all("table")[5].all("td")[3].text
+      page.find("input[type$='text']").set '2'
+      page.find("a[onclick*='login()']").click
+      page.visit "http://galois/dev_error.html"
+      galois = page.all("table")[5].all("td")[3].text
     end
     return {euclides: euclides, galois: galois}
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/status', to: 'linux_printers#status'
+  # get '/status', to: 'linux_printers#status'
+   get 'linux_printers/status'
 end
