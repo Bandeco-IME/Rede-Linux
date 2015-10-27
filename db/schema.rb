067c69f7bef70ae56dbe6fb609826590b4837bb0
root <root@template>
2015-10-27 15:07:03 -0200

    Fixed missing secret, updated home msg

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151020190935) do
 
-  create_table "linux_printers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "printers", force: :cascade do |t|
     t.string   "name"
     t.text     "status"
