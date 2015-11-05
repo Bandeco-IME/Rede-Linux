b53a8a54958948d4a98c37a9f5dae2560a868973
root <root@template>
2015-11-05 16:59:50 -0200

    Updated printer model and rake tasks

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -2,15 +2,26 @@ require 'cupsffi'
 
 class Printer < ActiveRecord::Base
 
-  
+  attr_accessor :cupsffi_object
   
   # DNS for the Cups server:
   @@cups_server = '192.168.240.15'
 
-  #network_printers = CupsPrinter.get_all_printer_names(:hostname => @@cups_server)
+  after_find do |printer|
+    printer.cupsffi_object = CupsPrinter.new(printer.name, :hostname => @@cups_server)
+  end
 
-  def update_status
-    status = CupsPrinter.new(name, :hostname => @@cups_server).state[:state].to_s
+  def accepting_jobs?
+    return self.cupsffi_object.attributes['printer-is-accepting-jobs']
   end
   
+  def update_status
+    if self.accepting_jobs?
+      new_status = @cupsffi_object.state[:state].to_s
+    else
+      yield
+    end
+
+    self.update_attributes(status: new_status, updated_at: Time.now) # Force timestamp upgrade
+  end
 end
