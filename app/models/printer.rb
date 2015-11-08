22c1a5ad36ca46e8a7a949cd0079a63ba5838cab
Gustavo Covas <gtcovas@gmail.com>
2015-11-08 14:33:43 -0200

    Added logic for checking cups server status

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -1,21 +1,22 @@
 require 'cupsffi'
 
 class Printer < ActiveRecord::Base
-
-  attr_accessor :cupsffi_object
   
   # DNS for the Cups server:
   @@cups_server = '192.168.240.15'
+  @@cups_server_up = system("ping -c 1 #{@@cups_server}")
 
-  after_find do |printer|
-    printer.cupsffi_object = CupsPrinter.new(printer.name, :hostname => @@cups_server)
-  end
-
+  @cupsffi_object = CupsPrinter.new(@name, :hostname => @@cups_server) if @@cups_server_up 
+    
   # Simple wrapper for the cupsffi gem:
   def accepting_jobs?
-    return self.cupsffi_object.attributes['printer-is-accepting-jobs']
+    if @@cups_server_up 
+      return self.cupsffi_object.attributes['printer-is-accepting-jobs']
+    else
+      return false
+    end
   end
-
+ 
   def update_status
     if self.accepting_jobs?
       new_status = @cupsffi_object.state[:state].to_s
@@ -24,7 +25,6 @@ class Printer < ActiveRecord::Base
     end
 
     # Force timestamp upgrade:
-    self.update_attributes(status: new_status, updated_at: Time.now)
-    
+    self.update_attributes(status: new_status, updated_at: Time.now)    
   end
 end
--- features/step_definitions/printers.rb
+++ features/step_definitions/printers.rb
@@ -4,7 +4,11 @@ require 'capybara/cucumber'
 
 Capybara.default_driver = :poltergeist
 
-Given(/^that I am on the home page$/) do
+Given(/^that we have "(.*?)" at the database$/) do |printer_name|
+  Printer.create(name: printer_name, status: "Undefined", url: "http://#{printer_name}/", error_url: "dstatus.cgi")
+end
+
+Given(/^I am on the home page$/) do
   page.visit root_path
   expect(page.has_content?).to be true
 end
