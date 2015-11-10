66c688a3eb647e701c87d22fe8ebec59f4a1a99c
root <root@template>
2015-11-10 20:48:43 -0200

    Ajuste dos validações

--- app/models/pre_signed_user.rb
+++ app/models/pre_signed_user.rb
@@ -6,11 +6,18 @@ class PreSignedUser < ActiveRecord::Base
 
   attr_accessor :password
 
-  validates :n_usp, presence: {message: "Campo do número USP em branco;"}
-  validates :name,  presence: true
-  validates :login, presence: true
-  validates :email, presence: true
-  validates :password, presence: true, confirmation: true
+  validates :n_usp, presence: {in: true, message: "Campo do número USP em branco;"},
+                    length:   {maximum: 30, message: "Número USP fornecido muito grande;"}
+  validates :name, presence:  {in: true, message: "Campo do nome em branco;"},
+                    length:   {maximum: 30, message: "Nome fornecido com mais de 30 caracteres;"}
+  validates :login, presence: {in: true, message: "Campo do login em branco;"},
+                    length:   {maximum: 10, message: "Login fornecido com mais de 10 caracteres;"},
+                    format:   {with: /\w+/i, message: "Login fornecido contem espaços em branco e/ou caracteres especiais;"}
+  validates :email, presence: {in: true, message: "Campo do email em branco;"},
+                    format:   {with: /\A[\w.%+-]+@[A-Z0-9.-]+\.([A-Z]{2,4})\Z/i, message: "Email fornecido inválido;"}
+  validates :password, presence:     {in: true, message: "Campo da senha em branco;"},
+                       confirmation: {in: true, message: "As senhas fornecidas não são iguais;"},
+                       length:       {in:6..20, message: "Senha deve ter no mínimo 6 e no máximo 20 caracteres;"}
 
   def bcrypt_password
     self.encrypt_password = BCrypt::Password.create(self.password)
