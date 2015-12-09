fbad6a2393b71f4ef8457ef6e22a8d08257bb8dc
root <root@template>
2015-12-09 14:45:15 -0200

    rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'pre-cadastro/', to: 'pre_signed_users#new'
-  get 'queue/', to: 'pre_signed_users#queue'
-  get 'pre_signed_users/', to: 'pre_signed_users#index'
+  get  'pre_signed_users/new/', to: 'pre_signed_users#new'
+  get  'pre_signed_users/queue/', to: 'pre_signed_users#queue'
+  get  'pre_signed_users/', to: 'pre_signed_users#index'
   post 'pre_signed_users/', to: 'pre_signed_users#create'
+  delete 'pre_signed_users/:id', to: 'pre_signed_users#destroy', as: :delete
+  root to: redirect('pre_signed_users/')
 end
