4b34acae9f118cf1590dfe50add21269b5c4f569
root <root@template>
2015-12-10 02:43:42 -0200

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get  'pre_signed_users/new/', to: 'pre_signed_users#new', as: :pre_signed_users_new
   get  'pre_signed_users/queue/', to: 'pre_signed_users#queue', as: :pre_signed_users_queue
-  get  'pre_signed_users/:id', to: 'pre_signed_users#destroy', as: :pre_signed_users_delete
+  delete  'pre_signed_users/:id', to: 'pre_signed_users#destroy', as: :pre_signed_users_delete
   post 'pre_signed_users/', to: 'pre_signed_users#create', as: :pre_signed_users
   get  'printer/status', to: 'printer#status', as: :printer_status
   get  'rede_linux/about', to: 'welcome#about', as: :welcome_about
